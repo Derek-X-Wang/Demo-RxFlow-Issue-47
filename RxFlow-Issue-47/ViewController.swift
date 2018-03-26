917b8625cf887ef3ca3fdf1dfec6909e57f20d9e
Derek Wang <derekxinzhewang@gmail.com>
2018-03-25 17:28:46 -0700

    Initial Commit

---
+++ RxFlow-Issue-47/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxFlow-Issue-47
+//
+//  Created by Xinzhe Wang on 3/25/18.
+//  Copyright © 2018 Xinzhe Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
